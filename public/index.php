4c8a95f0120b9fdba682b6275b65c2f0604c0156
Rachid <rachid@actived.fr>
2014-05-15 10:28:01 +0200

    Route exemple

--- public/index.php
+++ public/index.php
@@ -4,6 +4,10 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+$app->get('/', function() use($app) {
+	return $app->json(array(1,2,3));
+});
+
 // ... definitions
 $app['debug'] = true;
 $app->run();
