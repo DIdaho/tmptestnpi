0bc93dcef34f3f08dcc2a49af382c62aa2c9da8e
Rachid <rachid@actived.fr>
2014-05-23 11:41:26 +0200

    Activity list layout Wave detail WIP

--- public/js/controller/listWave.js
+++ public/js/controller/listWave.js
@@ -1,10 +1,17 @@
-function ctrlListWave($scope, $http, $routeParams, $injector){
+function ctrlListWave($scope, $http, $routeParams, $injector, $filter){
 
     $injector.invoke(ctrlList, this, {$scope: $scope});
 
+    //Get list
     $http.get('npi/' + $routeParams.id + '/waves').success(function(data){
-        console.log(data);
         $scope.list = data;
+        $scope.edit(data[0]);
+    });
+
+    //Get activities
+    $scope.activities = [];
+    $http.get('activity').success(function(data){
+        $scope.activities = data;
     });
 
     /**
@@ -16,5 +23,45 @@ function ctrlListWave($scope, $http, $routeParams, $injector){
         {
             item._ke_npi = $routeParams.id;
         }
+    }
+
+    //Activity selector
+    $scope.select = {
+        activity: null
+    }
+
+    /**
+     * Add an activity
+     */
+    $scope.addActivity = function(){
+        //Store ID
+        var id = $scope.select.activity._pk_activity;
+
+        //Reset selector
+        $scope.select.activity = null;
+
+        //Init item activities array
+        if(!$scope.item.activities)
+        {
+            $scope.item.activities = [];
+        }
+
+        //If activity already in array, ignore
+        if($filter('filter')($scope.item.activities, {_ke_activity: id}).length>0)
+        {
+            return;
+        }
+
+        //Add reference
+        $scope.item.activities.push({
+            _ke_activity: id
+        });
+    }
+
+    /**
+     * Remove an activity
+     */
+    $scope.removeActivity = function(){
+
     }
 }
