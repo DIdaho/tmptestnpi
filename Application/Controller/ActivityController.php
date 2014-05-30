a31ac48fcfe35580e773f7651bb894b0afbb1944
David <david@actived.fr>
2014-05-30 17:18:21 +0200

    Add constraint on Activity update and delete. Only if they don't have related wave.

--- Application/Controller/ActivityController.php
+++ Application/Controller/ActivityController.php
@@ -23,4 +23,39 @@ class ActivityController extends ControllerDefault {
         parent::__construct('activity');
     }
 
+    /**
+     * validation before activity deletion
+     * @param Application $app
+     * @param Request $request
+     * @param $id
+     * @throws \Exception
+     */
+    public function beforeDeleteAction(Application $app, Request $request, $id){
+        $targetRepository = $this->getRepository();
+        //check if current npi have related wave
+        $sql = "SELECT * FROM `waveactivity` WHERE _ke_activity = ".$targetRepository->cleanData($id);
+        /*@var $statement \PDOStatement*/
+        $statement = $targetRepository->query($sql);
+        if( false !== $statement && $statement->rowCount() > 0 ){
+            throw new \Exception('This activity have related Wave and can\'t be deleted');
+        }
+    }
+
+    /**
+     * validation before activity edition
+     * @param Application $app
+     * @param Request $request
+     * @param $id
+     * @throws \Exception
+     */
+    public function beforeUpdateAction(Application $app, Request $request, $id){
+        $targetRepository = $this->getRepository();
+        //check if current npi have related wave
+        $sql = "SELECT * FROM `waveactivity` WHERE _ke_activity = ".$targetRepository->cleanData($id);
+        /*@var $statement \PDOStatement*/
+        $statement = $targetRepository->query($sql);
+        if( false !== $statement && $statement->rowCount() > 0 ){
+            throw new \Exception('This activity have related Wave and can\'t be edited');
+        }
+    }
 }
